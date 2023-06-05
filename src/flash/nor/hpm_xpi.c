dbea4c77ae0a20f1729e232f4bf7cd86779a88c8
Ryan QIAN <jianghao.qian@hpmicro.com>
2023-06-05 09:21:37 +0800

    flash: nor: hpm_xpi: add controller base address as algo parameter
    - add controller base address as algo parameter
    
    Change-Id: I94d642d94d06fa7426b9ceebc706e96d52510c37
    Signed-off-by: Ryan QIAN <jianghao.qian@hpmicro.com>

--- src/flash/nor/hpm_xpi.c
+++ src/flash/nor/hpm_xpi.c
@@ -38,7 +38,7 @@ typedef struct hpm_xpi_priv {
 static int hpm_xpi_probe(struct flash_bank *bank)
 {
 	int retval = ERROR_OK;
-	struct reg_param reg_params[5];
+	struct reg_param reg_params[6];
 	hpm_xpi_priv_t *xpi_priv;
 	int xlen;
 	hpm_flash_info_t flash_info = {0};
@@ -87,13 +87,15 @@ static int hpm_xpi_probe(struct flash_bank *bank)
 	init_reg_param(&reg_params[1], "a1", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[2], "a2", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[3], "a3", xlen, PARAM_OUT);
-	init_reg_param(&reg_params[4], "ra", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[4], "a4", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[5], "ra", xlen, PARAM_OUT);
 	buf_set_u64(reg_params[0].value, 0, xlen, bank->base);
 	buf_set_u64(reg_params[1].value, 0, xlen, xpi_priv->header);
 	buf_set_u64(reg_params[2].value, 0, xlen, xpi_priv->opt0);
 	buf_set_u64(reg_params[3].value, 0, xlen, xpi_priv->opt1);
-	buf_set_u64(reg_params[4].value, 0, xlen, wa->address + FLASH_INIT + 4);
-	retval = target_run_algorithm(target, 0, NULL, 5, reg_params,
+	buf_set_u64(reg_params[4].value, 0, xlen, xpi_priv->io_base);
+	buf_set_u64(reg_params[5].value, 0, xlen, wa->address + FLASH_INIT + 4);
+	retval = target_run_algorithm(target, 0, NULL, 6, reg_params,
 			wa->address, wa->address + FLASH_INIT + 4, 500, NULL);
 	if (retval != ERROR_OK) {
 		LOG_ERROR("Failed to execute run algorithm: %d", retval);
@@ -184,7 +186,7 @@ static int hpm_xpi_auto_probe(struct flash_bank *bank)
 static int hpm_xpi_write(struct flash_bank *bank, const uint8_t *buffer,
 	uint32_t offset, uint32_t count)
 {
-	struct reg_param reg_params[5];
+	struct reg_param reg_params[6];
 	int retval = ERROR_OK;
 	struct target *target = bank->target;
 	struct working_area *data_wa = NULL;
@@ -220,13 +222,15 @@ static int hpm_xpi_write(struct flash_bank *bank, const uint8_t *buffer,
 	init_reg_param(&reg_params[1], "a1", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[2], "a2", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[3], "a3", xlen, PARAM_OUT);
-	init_reg_param(&reg_params[4], "ra", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[4], "a4", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[5], "ra", xlen, PARAM_OUT);
 	buf_set_u64(reg_params[0].value, 0, xlen, bank->base);
 	buf_set_u64(reg_params[1].value, 0, xlen, xpi_priv->header);
 	buf_set_u64(reg_params[2].value, 0, xlen, xpi_priv->opt0);
 	buf_set_u64(reg_params[3].value, 0, xlen, xpi_priv->opt1);
-	buf_set_u64(reg_params[4].value, 0, xlen, wa->address + FLASH_INIT + 4);
-	retval = target_run_algorithm(target, 0, NULL, 5, reg_params,
+	buf_set_u64(reg_params[4].value, 0, xlen, xpi_priv->io_base);
+	buf_set_u64(reg_params[5].value, 0, xlen, wa->address + FLASH_INIT + 4);
+	retval = target_run_algorithm(target, 0, NULL, 6, reg_params,
 			wa->address, wa->address + FLASH_INIT + 4, 500, NULL);
 	if (retval != ERROR_OK) {
 		LOG_ERROR("Failed to execute run algorithm: %d", retval);
@@ -325,7 +329,7 @@ err:
 static int hpm_xpi_erase(struct flash_bank *bank, unsigned int first, unsigned int last)
 {
 	int retval = ERROR_OK;
-	struct reg_param reg_params[5];
+	struct reg_param reg_params[6];
 	struct target *target = bank->target;
 	struct working_area *wa = NULL;
 	int xlen;
@@ -357,13 +361,15 @@ static int hpm_xpi_erase(struct flash_bank *bank, unsigned int first, unsigned i
 	init_reg_param(&reg_params[1], "a1", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[2], "a2", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[3], "a3", xlen, PARAM_OUT);
-	init_reg_param(&reg_params[4], "ra", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[4], "a4", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[5], "ra", xlen, PARAM_OUT);
 	buf_set_u64(reg_params[0].value, 0, xlen, bank->base);
 	buf_set_u64(reg_params[1].value, 0, xlen, xpi_priv->header);
 	buf_set_u64(reg_params[2].value, 0, xlen, xpi_priv->opt0);
 	buf_set_u64(reg_params[3].value, 0, xlen, xpi_priv->opt1);
-	buf_set_u64(reg_params[4].value, 0, xlen, wa->address + FLASH_INIT + 4);
-	retval = target_run_algorithm(target, 0, NULL, 5, reg_params,
+	buf_set_u64(reg_params[4].value, 0, xlen, xpi_priv->io_base);
+	buf_set_u64(reg_params[5].value, 0, xlen, wa->address + FLASH_INIT + 4);
+	retval = target_run_algorithm(target, 0, NULL, 6, reg_params,
 			wa->address, wa->address + FLASH_INIT + 4, 500, NULL);
 	if (retval != ERROR_OK) {
 		LOG_ERROR("Failed to execute run algorithm: %d", retval);
@@ -411,7 +417,7 @@ err:
 static int hpm_xpi_erase_chip(struct flash_bank *bank)
 {
 	int retval = ERROR_OK;
-	struct reg_param reg_params[5];
+	struct reg_param reg_params[6];
 	struct target *target = bank->target;
 	struct working_area *wa = NULL;
 	int xlen;
@@ -444,13 +450,15 @@ static int hpm_xpi_erase_chip(struct flash_bank *bank)
 	init_reg_param(&reg_params[1], "a1", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[2], "a2", xlen, PARAM_OUT);
 	init_reg_param(&reg_params[3], "a3", xlen, PARAM_OUT);
-	init_reg_param(&reg_params[4], "ra", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[4], "a4", xlen, PARAM_OUT);
+	init_reg_param(&reg_params[5], "ra", xlen, PARAM_OUT);
 	buf_set_u64(reg_params[0].value, 0, xlen, bank->base);
 	buf_set_u64(reg_params[1].value, 0, xlen, xpi_priv->header);
 	buf_set_u64(reg_params[2].value, 0, xlen, xpi_priv->opt0);
 	buf_set_u64(reg_params[3].value, 0, xlen, xpi_priv->opt1);
-	buf_set_u64(reg_params[4].value, 0, xlen, wa->address + FLASH_INIT + 4);
-	retval = target_run_algorithm(target, 0, NULL, 5, reg_params,
+	buf_set_u64(reg_params[4].value, 0, xlen, xpi_priv->io_base);
+	buf_set_u64(reg_params[5].value, 0, xlen, wa->address + FLASH_INIT + 4);
+	retval = target_run_algorithm(target, 0, NULL, 6, reg_params,
 			wa->address, wa->address + FLASH_INIT + 4, 500, NULL);
 	if (retval != ERROR_OK) {
 		LOG_ERROR("Failed to execute run algorithm: %d", retval);
